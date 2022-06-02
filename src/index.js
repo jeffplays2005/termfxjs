6ade98d5f9ea1d9ba142301e539d0c39f787190d
jeffplays2005 <61447509+jeffplays2005@users.noreply.github.com>
2022-06-02 20:47:33 +1200

    Updated to match current npm version
    Added more functions/removed some comments.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 class New {
-  // constructor(){
-  //   this.variables = {};
-  //   this.functions = {};
-  //
-  //
-  // }
   constructor(){
     this.commands = {};
-    this._ErrFunctionAlreadyRegistered = function (){
-      throw Error("function with the same name has already been registered")
-    }
   }
   RegisterVariable(name, value){
     name = "$" + name;
 
     var ok = this.commands[name];
     if(ok){
-      return this._ErrFunctionAlreadyRegistered();
+      throw new Error("function with the same name has already been registered")
     }
     this.commands[name] = value;
+  }
+  RegisterFunction(name, func){
+    name += "()";
+
+    var ok = this.commands[name];
+    if(ok){
+      throw new Error("function with the same name has already been registered")
+    }
+
+    this.commands[name] = func;
   }
 }
 
