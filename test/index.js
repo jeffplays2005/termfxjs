af41691ce8f79af1119955bc14ae9134b7a3443d
jeffplays2005 <61447509+jeffplays2005@users.noreply.github.com>
2022-06-12 20:49:22 +1200

    Changed delay function and removed requirement of a extra package.

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 // testing dup
 (async() => {
   registry.RegisterVariable('foo', 'bar');
-  registry.RegisterFunction('sleep', async function(x){ await delay(x) });
+  registry.RegisterFunction('sleep', async function(x){ return new Promise(resolve => setTimeout(resolve, x)) });
   registry.RegisterFunction('clear', function(){ console.clear() });
 
   var text = await fs.readFileSync(__dirname+'/test.tfx')
